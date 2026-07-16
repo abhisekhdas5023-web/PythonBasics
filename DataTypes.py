c3d762a3a5c8ecefc444412b83507fbce51552cb
Abhisekh Das <abhisekh.das@gyansys.com>
2026-07-16 20:04:51 +0530

    Added Python variables and casting examples

---
+++ DataTypes.py
@@ -0,0 +1,57 @@
+# BuildIn Data Types
+
+# Python has the following data types built-in by default, in these categories:
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+x = "Hello World"
+print(x)
+
+x = 20
+print(x)
+
+x = 20.5
+print(x)
+
+x = 1j	
+print(x)
+
+x = ["apple", "banana", "cherry"]		
+print(x)
+
+x = ("apple", "banana", "cherry")		
+print(x)
+
+x = range(6)		
+print(x)
+
+x = {"name" : "John", "age" : 36}	
+print(x)
+
+x = {"apple", "banana", "cherry"}	
+print(x)
+
+x = frozenset({"apple", "banana", "cherry"})	
+print(x)
+
+x = True	
+print(x)
+
+x = b"Hello"	
+print(x)
+
+x = bytearray(5)	
+print(x)
+
+x = memoryview(bytes(5))	
+print(x)
+
+x = None	
+print(x)
---
+++ Variables.py
@@ -0,0 +1,41 @@
+# What is Variables in Python?
+# Variables are container for storing data values.
+
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it. 
+
+x = 400000   
+y = "Abhisekh"
+print(x)   
+print(y)     
+
+
+# What is Casting?
+# If you want to specify the data type of a variable,this can be done with casting.
+
+
+a = str(5)
+b = int(5)
+c = float(5)
+
+print(a)
+print(b)
+print(c)
+
+
+
+# Types of variable :
+# you can get the data type of a variable with the type() function. 
+
+y = 10
+z = "Abhi"
+
+print(type(y))
+print(type(z))
+
+
+# String Variables can be declared using single or double quotes.
+
+#  Variables names are case sensitive
+# For Example : a=10, A=9.99
+# Here in this scenario capital A will not overwrite a.
